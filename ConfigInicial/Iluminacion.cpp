66b5ad502593e9c4da4b13eb06b59b2c97ea34b0
Fernando Perez <fer658798@gmail.com>
2025-10-12 22:09:38 -0600

    Practica 8

--- ConfigInicial/Iluminacion.cpp
+++ ConfigInicial/Iluminacion.cpp
@@ -1,6 +1,6 @@
 /*
-* Previo 8
-* Fecha de entrega: 29 de septiembre de 2025
+* Práctica 8
+* Fecha de entrega: 12 de octubre de 2025
 * Fernando Pérez Cruz
 * 422022569
 */
@@ -45,16 +45,26 @@ GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
 
 
-// Light attributes
-glm::vec3 lightPos(0.5f, 0.5f, 2.5f);
-float movelightPos = 0.0f;
+// ==== Sun attributes ====
+glm::vec3 sunPos(0.0f, 0.0f, 0.0f);
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
 bool activanim = false;
 
-// otra fuente de iluminación
-glm::vec3 lightPos2(2.0f, 1.5, 2.5f);
+float sun_radius = 12.0f;
+float sun_height_radius = 5.0f; 
+float moveSunPos = 0.0f;
+
+
+// ==== Moon attributes ====
+glm::vec3 moonPos(0.0f, 0.0f, 0.0f);
+float moon_radius = 12.0f;
+float moon_height_radius = 5.0f; 
+float moveMoonPos = 0.0f;
+
+// ==== Control de día/noche ====
+bool isDay = true;
 
 int main()
 {
@@ -68,7 +78,7 @@ int main()
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Previo 7 Fernando Perez", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Practica 8 Fernando Perez", nullptr, nullptr);
 
     if (nullptr == window)
     {
@@ -109,13 +119,18 @@ int main()
     Shader lampshader("Shader/lamp.vs", "Shader/lamp.frag");
     Shader lightingShader("Shader/lighting.vs", "Shader/lighting.frag");
 
-
-
     // Load models
-    Model red_dog((char*)"Models/RedDog.obj");
+    Model dog((char*)"Models/RedDog.obj");
+    Model forest((char*)"Models/forest.obj");
+    Model arlo((char*)"Models/arlo.obj");
+    Model trex((char*)"Models/dino2.obj");
+    Model mesaPicnic((char*)"Models/mesapicnic.obj");
+    Model fence((char*)"Models/fence.obj");
+    Model agave((char*)"Models/agave.obj");
 
-    // otro modelo
-    Model dino((char*)"Models/dino2.obj");
+    // === Sun and Moon ===
+    Model sun((char*)"Models/sun.obj");
+    Model moon((char*)"Models/moon.obj");
 
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
@@ -214,29 +229,86 @@ int main()
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
+        // ==== Calculando la posición del sol y la luna ====
+
+        float angle_degrees = glm::degrees(moveSunPos);
+        float clamped_angle = glm::clamp(angle_degrees, 0.0f, 180.0f);
+        float clamped_radians = glm::radians(clamped_angle);
+
+        sunPos.x = sun_radius * glm::cos(clamped_radians);
+        sunPos.y = sun_height_radius * glm::sin(clamped_radians); 
+        sunPos.z = -10.0f;
+
+        float moon_angle_degrees = glm::degrees(moveMoonPos);
+        float moon_clamped_angle = glm::clamp(moon_angle_degrees, 0.0f, 180.0f);
+        float moon_clamped_radians = glm::radians(moon_clamped_angle);
+
+        moonPos.x = -5.0f; 
+        moonPos.y = moon_height_radius * glm::sin(moon_clamped_radians);
+        moonPos.z = moon_radius * glm::cos(moon_clamped_radians);
+
         // Check and call events
         glfwPollEvents();
         DoMovement();
 
+        // ==== Propiedades para las luces y color de fondo ====
+        glm::vec3 sun_ambient, sun_diffuse, sun_specular;
+        glm::vec3 moon_ambient, moon_diffuse, moon_specular;
+        glm::vec4 clearColor;
+
+        // Para el día
+        if (isDay) {
+            clearColor = glm::vec4(0.53f, 0.81f, 0.98f, 1.0f);
+
+            sun_ambient = glm::vec3(0.5f, 0.5f, 0.4f);  
+            sun_diffuse = glm::vec3(1.0f, 1.0f, 0.9f);  
+            sun_specular = glm::vec3(1.0f, 1.0f, 1.0f);
+
+            moon_ambient = glm::vec3(0.0f, 0.0f, 0.0f);
+            moon_diffuse = glm::vec3(0.0f, 0.0f, 0.0f);
+            moon_specular = glm::vec3(0.0f, 0.0f, 0.0f);
+        }
+        // Para la noche
+        else {
+            clearColor = glm::vec4(0.05f, 0.05f, 0.15f, 1.0f); 
+
+            sun_ambient = glm::vec3(0.0f, 0.0f, 0.0f);
+            sun_diffuse = glm::vec3(0.0f, 0.0f, 0.0f);
+            sun_specular = glm::vec3(0.0f, 0.0f, 0.0f);
+
+            moon_ambient = glm::vec3(0.1f, 0.1f, 0.2f);
+            moon_diffuse = glm::vec3(0.3f, 0.3f, 0.4f);
+            moon_specular = glm::vec3(0.4f, 0.4f, 0.5f);
+        }
+
+
         // Clear the colorbuffer
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-        // ======= LUZ 1 ==========
         
         lightingShader.Use();
-        GLint lightPosLoc = glGetUniformLocation(lightingShader.Program, "light.position");
+
         GLint viewPosLoc = glGetUniformLocation(lightingShader.Program, "viewPos");
-        glUniform3f(lightPosLoc, lightPos.x + movelightPos, lightPos.y + movelightPos, lightPos.z + movelightPos);
         glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
 
+        // Luces usadas
+        glUniform1i(glGetUniformLocation(lightingShader.Program, "numLights"), 2);
 
-        // Set lights properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.3f, 0.3f, 0.3f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.2f, 0.7f, 0.8f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.3f, 0.6f, 0.4f);
+        //glm::vec3 L1 = sunPos + glm::vec3(moveSunPos);
+        //glUniform3fv(glGetUniformLocation(lightingShader.Program, "lights[0].position"), 1, glm::value_ptr(L1));
+        glm::vec3 L1 = sunPos;
+        glUniform3fv(glGetUniformLocation(lightingShader.Program, "lights[0].position"), 1, glm::value_ptr(L1));
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[0].ambient"), sun_ambient.x, sun_ambient.y, sun_ambient.z);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[0].diffuse"), sun_diffuse.x, sun_diffuse.y, sun_diffuse.z);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[0].specular"), sun_specular.x, sun_specular.y, sun_specular.z);
 
+        //glm::vec3 L2 = moonPos + glm::vec3(moveMoonPos);
+        //glUniform3fv(glGetUniformLocation(lightingShader.Program, "lights[1].position"), 1, glm::value_ptr(L2));
+        glm::vec3 L2 = moonPos; // Simplemente usa la nueva posición
+        glUniform3fv(glGetUniformLocation(lightingShader.Program, "lights[1].position"), 1, glm::value_ptr(L2));
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[1].ambient"), moon_ambient.x, moon_ambient.y, moon_ambient.z);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[1].diffuse"), moon_diffuse.x, moon_diffuse.y, moon_diffuse.z);
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "lights[1].specular"), moon_specular.x, moon_specular.y, moon_specular.z);
 
         glm::mat4 view = camera.GetViewMatrix();
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
@@ -249,31 +321,125 @@ int main()
         glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.6f);
 
 
-        // ======= LUZ 2 =========
+        // ==== Cargando escenario ====
+        
+        // Drawing forest
+        glm::mat4 model(1);
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        forest.Draw(lightingShader);
 
-        GLint lightPosLoc2 = glGetUniformLocation(lightingShader.Program, "light2.position");
-        GLint viewPosLoc2 = glGetUniformLocation(lightingShader.Program, "viewPos");
-        glUniform3f(lightPosLoc2, lightPos2.x + movelightPos, lightPos2.y + movelightPos, lightPos2.z + movelightPos);
-        glUniform3f(viewPosLoc2, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z);
+        // Arlo the dinosaur
+        model = glm::mat4(1);
+        model = glm::rotate(model, glm::radians(12.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        arlo.Draw(lightingShader);
 
+        // Another dino
+        model = glm::mat4(1);
+        model = glm::rotate(model, glm::radians(10.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        trex.Draw(lightingShader);
 
-        // Set lights properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light2.ambient"), 0.3f, 0.3f, 0.3f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light2.diffuse"), 0.5f, 0.3f, 0.5f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light2.specular"), 0.4f, 0.4f, 0.6f);
+        // Our dog
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(0.0f, 0.6f, 2.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        dog.Draw(lightingShader);
 
+        // Picnic table
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        mesaPicnic.Draw(lightingShader);
 
-        view = camera.GetViewMatrix();
-        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        // Some fences
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        fence.Draw(lightingShader);
 
-        // Set material properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"), 0.1f, 0.1f, 0.5f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"), 0.9f, 0.3f, 0.6f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 1.0f, 0.5f, 0.2f);
-        glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.2f);
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(4.5f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        fence.Draw(lightingShader);
 
+        // Some plants
+        model = glm::mat4(1);
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.0f, 0.0f, -1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.0f, 0.0f, -1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-4.0f, 0.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-4.0f, 0.0f, -1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-4.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-3.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-2.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-0.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(1.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.0f, 0.0f, -2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        agave.Draw(lightingShader);
+
+        // ==== Termina carga de escenario
+
+        /*
         // Draw the loaded model
         glm::mat4 model(1);
         model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
@@ -291,31 +457,54 @@ int main()
         glBindVertexArray(VAO);
         dino.Draw(lightingShader);
 
-        
+        */
 
         glBindVertexArray(0);
-
-
 
 
         lampshader.Use();
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos + movelightPos);
-        model = glm::scale(model, glm::vec3(0.3f));
-        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
 
-        // luz 2
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos2 + movelightPos);
-        model = glm::scale(model, glm::vec3(0.3f));
-        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        // ==== Dibujo del Sol y la Luna ====
+        if ( isDay ) {
+            // Lámpara de la luz 1 (Sol)
+            glm::mat4 lampM = glm::mat4(1.0f);
+            lampM = glm::translate(lampM, L1);
+            lampM = glm::scale(lampM, glm::vec3(0.5f));
+            glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(lampM));
+            
+            sun.Draw(lampshader);
+        }
+        else {
+            // Lámpara de la luz 2 (Luna)
+            glm::mat4 lampM = glm::mat4(1.0f);
+            lampM = glm::translate(lampM, L2);
+            lampM = glm::scale(lampM, glm::vec3(0.5f));
+            glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(lampM));
+            
+
+            moon.Draw(lampshader); 
+        }
+
+        /* ==== Luces en cubos ====
+        // Lámpara de la luz 1
+        glm::mat4 lampM = glm::mat4(1.0f);
+        lampM = glm::translate(lampM, L1);
+        lampM = glm::scale(lampM, glm::vec3(0.2f));
+        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(lampM));
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // Lámpara de la luz 2
+        lampM = glm::mat4(1.0f);
+        lampM = glm::translate(lampM, L2);
+        lampM = glm::scale(lampM, glm::vec3(0.2f));
+        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(lampM));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        */
+
         glBindVertexArray(0);
 
         // Swap the buffers
@@ -383,15 +572,29 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
     }
 
     if (keys[GLFW_KEY_O])
-    {
-       
-        movelightPos += 0.1f;
+    {  
+        //moveSunPos += 0.1f;
+        if (isDay)
+            moveSunPos += glm::radians(1.0f);
+        else
+            moveMoonPos += glm::radians(1.0f);
+
     }
 
     if (keys[GLFW_KEY_L])
     {
         
-        movelightPos -= 0.1f;
+        //moveSunPos -= 0.1f;
+        if (isDay)
+            moveSunPos -= glm::radians(1.0f);
+        else
+            moveMoonPos -= glm::radians(1.0f);
+
+    }
+
+    // ==== Alternamos entre día y noche con la tecla P ===
+    if (key == GLFW_KEY_P && action == GLFW_PRESS) {
+        isDay = !isDay;
     }
 
 
--- ConfigInicial/Shader/lighting.frag
+++ ConfigInicial/Shader/lighting.frag
@@ -10,7 +10,6 @@ struct Material
 struct Light
 {
     vec3 position;
-    
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -24,52 +23,42 @@ out vec4 color;
 
 uniform vec3 viewPos;
 uniform Material material;
-uniform Light light;
 
-
-// Otra luz
-uniform Light light2;
+// ==== Luces ====
+uniform int numLights;
+uniform Light lights[2];
 
 // para recibir la textura
 uniform sampler2D texture_diffuse;
+
+vec3 shade(Light light, vec3 norm, vec3 viewDir) {
+
+    // Ambient
+    vec3 ambient = light.ambient * material.diffuse;
+
+    // Diffuse
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * material.diffuse;
+
+    // Specular
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
+
+    return ambient + diffuse + specular;
+}
 
 void main()
 {
     vec3 norm = normalize(Normal);
     vec3 viewDir = normalize(viewPos - FragPos);
 
-    // ==== Luz 1 ====    
-    
-    // Ambient
-    vec3 ambient = light.ambient *material.diffuse;
-    
-    // Diffuse
-    //vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * material.diffuse;
-    
-    // Specular
-    //vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);
-    
-    //vec3 result = ambient + diffuse + specular;
-    //color = vec4(result, 1.0f) * texture(texture_diffuse, TexCoords);
+    vec3 lighting = vec3(0.0);
 
+    for ( int i = 0; i < numLights; i++) {
+        lighting += shade(lights[i], norm, viewDir);
+    }
 
-    // ===== Luz 2 =====
-    vec3 lightDir2 = normalize(light2.position - FragPos);
-    float diff2 = max(dot(norm, lightDir2), 0.0);
-    vec3 ambient2  = light2.ambient * material.diffuse;
-    vec3 diffuse2  = light2.diffuse * diff2 * material.diffuse;
-    vec3 reflectDir2 = reflect(-lightDir2, norm);
-    float spec2 = pow(max(dot(viewDir, reflectDir2), 0.0), material.shininess);
-    vec3 specular2 = light2.specular * (spec2 * material.specular);
-
-    // Suma de ambas luces
-    vec3 result = ambient + diffuse + specular + ambient2 + diffuse2 + specular2;
-
-    color = vec4(result, 1.0) * texture(texture_diffuse, TexCoords);
+    color = vec4(lighting, 1.0) * texture(texture_diffuse, TexCoords);
 }
